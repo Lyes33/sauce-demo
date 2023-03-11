b1f3fc507300e2837f381b614afd01b72b15c052
Lyes33 <bob.lyes@live.com>
2023-03-11 22:15:17 +0100

    create repo + report

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,19 @@
+///<reference typres="cypress">
+
+describe('Test suite sauce demo', () => {
+  beforeEach(()=>{
+    cy.visit('/')
+  })
+
+  it('Varifier la présence des éléments sur la page sauce demo', () => {
+    cy.get('.login_logo').should('be.visible').and('contain.text', 'Swag Labs')
+    cy.get('[data-test="login-button"]').should('be.visible').and('be.enabled')
+    
+  })
+   it('Connexion sur le site sauce demo', () => {
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    
+  })
+})
