58eb237100f197c6d729015dedb6df385fdda06e
Lyes33 <bob.lyes@live.com>
2023-03-12 16:14:20 +0100

    remove comments

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -9,17 +9,11 @@ describe('Test suite sauce demo', () => {
     //Vérifier
     cy.log('verif test one')
     cy.contains('h1', 'Bienvenue').should('be.visible')
-   /* cy.get('.login_logo').should('be.visible').and('contain.text', 'Swag Labs')
-    cy.get('[data-test="login-button"]').should('be.visible').and('be.enabled')*/
     
   })
    it('Connexion sur le site sauce demo', () => {
     cy.log('verif test tow')
     cy.get('#btn').should('contain','Submit').and('be.visible')
-    
-    /*cy.get('[data-test="username"]').type('standard_user')
-    cy.get('[data-test="password"]').type('secret_sauce')
-    cy.get('[data-test="login-button"]').click()*/
     
   })
 })
